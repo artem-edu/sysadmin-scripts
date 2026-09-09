448d24d2292ad67c6288ea3c85419c89c0aeb75f
Artem Kaverza <kav167@campus.mephi.ru>
2026-09-10 01:34:39 +0300

    доработка скрипта, добавлены базовые параметры пользователя

--- script.sh
+++ script.sh
@@ -1,14 +1,32 @@
 #!/usr/bin/env bash
-USER_NAME="$1"
+USER_NAME="${1:-}"
 SCRIPT_NAME=$(basename "$0")
 
 if [ -z "$USER_NAME" ]; then
+	echo "Ошибка: не казано имя пользователя." >&2
 	echo "Использование $SCRIPT_NAME имя_пользователя" >&2
 	exit 1;
 fi
 
-TARGET_DIR="$HOME/managed-users/$USERNAME"
+TARGET_DIR="$HOME/managed-users/$USER_NAME"
+BASHRC_PATH="$TARGET_DIR/.bashrc"
+
 mkdir -p "$TARGET_DIR"
-echo "# настройки $USER_NAME" > "$TARGET_DIR/.bashrc"
+
+cat << EOF >> "$BASHRC_PATH"
+# настройки $USER_NAME"
+
+# Короткая команда обновления ОС
+alias update='sudo apt update && sudo apt upgrade -y'
+
+# Терминальный редактор по умолчанию vim
+export EDITOR='vi'
+
+# Функция создат папку и сразу войти в нее
+mkcd() {
+	mkdir -p "\$1" && cd "\$1"
+}
+EOF
+
 echo "Добро пожаловать, $USER_NAME!"
 echo "$(date): создан пользовательский каталог для $USER_NAME" >> setup.log
